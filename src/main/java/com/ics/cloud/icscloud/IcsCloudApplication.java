74e6817e08e42a87d51ad9491f9b570de396db83
长歌行 <wxpro@vip.qq.com>
2019-12-20 17:59:17 +0800

    init

---
+++ src/main/java/com/ics/cloud/icscloud/IcsCloudApplication.java
@@ -0,0 +1,13 @@
+package com.ics.cloud.icscloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcsCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcsCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ics/cloud/icscloud/IcsCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ics.cloud.icscloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcsCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
