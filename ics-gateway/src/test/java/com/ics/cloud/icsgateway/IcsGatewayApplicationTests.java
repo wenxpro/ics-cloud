48f6ec40899d7e4c0964f705ca270a7a2745c4ca
长歌行 <wxpro@vip.qq.com>
2019-12-31 18:12:37 +0800

    mod:依赖修改

--- ics-gateway/src/test/java/com/ics/cloud/icsgateway/IcsGatewayApplicationTests.java
+++ ics-gateway/src/test/java/com/ics/cloud/icsgateway/IcsGatewayApplicationTests.java
@@ -1,13 +1,13 @@
 package com.ics.cloud.icsgateway;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class IcsGatewayApplicationTests {
+public class IcsGatewayApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
--- ics-monitor/src/test/java/com/ics/cloud/icsmonitor/IcsMonitorApplicationTests.java
+++ ics-monitor/src/test/java/com/ics/cloud/icsmonitor/IcsMonitorApplicationTests.java
@@ -1,13 +1,13 @@
 package com.ics.cloud.icsmonitor;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class IcsMonitorApplicationTests {
+public class IcsMonitorApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
